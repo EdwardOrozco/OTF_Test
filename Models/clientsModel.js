ae4baa7db08a7f33b48d005cbbc01192eecf1e82
Edward Orozco <edwardorozco09@hotmail.com>
2022-04-28 02:52:33 -0500

    Const clientsModel created
    Const clientsModel created

--- Models/clientsModel.js
+++ Models/clientsModel.js
@@ -1,6 +1,6 @@
 const hubspot = require('@hubspot/api-client');
 
-const hubspotClient = new hubspot.Client({"apiKey":"99011b81-c161-4ebd-b7f9-52f222ecea04"});
+const hubspotClient = new hubspot.Client({ "apiKey": "99011b81-c161-4ebd-b7f9-52f222ecea04" });
 
 const tableIdOrName = "developer_test_4";
 const sort = undefined;
@@ -8,17 +8,21 @@ const after = undefined;
 const limit = undefined;
 const properties = undefined;
 
-async function dbRequest (){
 
-try {
-  const apiResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRows(tableIdOrName, sort, after, limit, properties);
-  console.log(JSON.stringify(apiResponse.results, null, 2));
-} catch (e) {
-  e.message === 'HTTP request failed'
-    ? console.error(JSON.stringify(e.response, null, 2))
-    : console.error(e)
+const clientsModel = {
+
+  getAllRows: async function () {
+
+    try {
+      const apiResponse = await hubspotClient.cms.hubdb.rowsApi.getTableRows(tableIdOrName, sort, after, limit, properties);
+      console.log(JSON.stringify(apiResponse, null, 2));
+    } catch (e) {
+      e.message === 'HTTP request failed'
+        ? console.error(JSON.stringify(e.response, null, 2))
+        : console.error(e)
+    }
+
+  }
 }
 
-}
-
-dbRequest();
+clientsModel.getAllRows();
